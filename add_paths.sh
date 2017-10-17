3e73f9ec1faca4af7c7a9769c435e22a3cd8d02e
Jakob Jordan <j.jordan@fz-juelich.de>
2017-10-17 19:22:01 +0200

    Add script to update paths

---
+++ add_paths.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+export PATH=$HOME/opt/MUSIC/install/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/opt/MUSIC/install/lib:$LD_LIBRARY_PATH
+export PATH=$HOME/opt/music-adapters/install/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/opt/music-adapters/install/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$HOME/opt/nest-simulator/install/lib/python3.5/site-packages:$PYTHONPATH
