b5b733197261edda9832aeddb4e04a57b241ab9e
Jakob Jordan <j.jordan@fz-juelich.de>
2017-10-24 18:52:30 +0200

    Update example4

--- example4/nest_sim.py
+++ example4/nest_sim.py
@@ -9,6 +9,12 @@ comm = MPI.COMM_WORLD
 
 
 def get_current_offset(weight, rate, tau_m, tau_syn, C_m):
+    """
+    Computes the mean input expected from an input with rate `rate`
+    and weight `weight` for a neuron with parameters `tau_m`,
+    `tau_syn` and `C_m` (see Campbell's theorem).
+
+    """
     return weight / C_m * rate * tau_m * tau_syn * 1e-3
 
 
@@ -30,7 +36,7 @@ music_in_proxy = nest.Create('music_event_in_proxy', 1, {'port_name': 'in'})
 music_out_proxy = nest.Create('music_event_out_proxy', 1, {'port_name': 'out'})
 
 neuron_left = nest.Create('iaf_psc_exp', 1, {
-    'E_L': -60.3 + get_current_offset(J, max_rate / 2., tau_m, tau_syn, C_m),
+    'E_L': -60.3 + get_current_offset(J, max_rate / 2., tau_m, tau_syn, C_m),  # add additional 0.3mV to make the left neuron a bit less excitable
     'V_th': -60., 'tau_m': tau_m, 'tau_syn_ex': tau_syn, 'tau_syn_in': tau_syn, 'C_m': C_m
 })
 neuron_right = nest.Create('iaf_psc_exp', 1, {
@@ -42,17 +48,15 @@ neuron_command = nest.Create('iaf_psc_exp', 1, {'E_L': -60., 'V_th': -60., 'tau_
 
 sd = nest.Create('spike_detector')
 
-sd_ev = nest.Create('spike_detector')
-
 mv_left = nest.Create('multimeter', 1, {'record_from': ['V_m']})
 mv_right = nest.Create('multimeter', 1, {'record_from': ['V_m']})
 mv_command = nest.Create('multimeter', 1, {'record_from': ['V_m']})
 
-nest.Connect(music_in_proxy, neuron_left, syn_spec={'weight': -200.})
-nest.Connect(music_in_proxy, neuron_right, syn_spec={'weight': 200.})
+nest.Connect(music_in_proxy, neuron_left, syn_spec={'weight': -J})
+nest.Connect(music_in_proxy, neuron_right, syn_spec={'weight': J})
 
-nest.Connect(neuron_left, neuron_command, syn_spec={'weight': 200.})
-nest.Connect(neuron_right, neuron_command, syn_spec={'weight': -200.})
+nest.Connect(neuron_left, neuron_command, syn_spec={'weight': J})
+nest.Connect(neuron_right, neuron_command, syn_spec={'weight': -J})
 
 nest.Connect(neuron_command, music_out_proxy, syn_spec={'receptor_type': 0})
 
@@ -64,15 +68,11 @@ nest.Connect(mv_left, neuron_left)
 nest.Connect(mv_right, neuron_right)
 nest.Connect(mv_command, neuron_command)
 
-nest.Connect(music_in_proxy, sd_ev)
-
 comm.Barrier()  # necessary to synchronize with MUSIC
 
 nest.Simulate(simtime)
 
 # plot results
-
-print('event rate', nest.GetStatus(sd_ev, 'n_events')[0] / simtime * 1e3)
 
 spikes = nest.GetStatus(sd, 'events')[0]
 
@@ -100,4 +100,4 @@ ax2.plot(vm_command['times'], vm_command['V_m'], 'm', label='command')
 
 plt.legend()
 
-plt.show()
+fig.savefig('nest_output.png', dpi=300)
