9a240a34569338cbdc06309a1b16a538f6aaaee8
Jakob Jordan <j.jordan@fz-juelich.de>
2017-10-24 17:26:50 +0200

    Fix example4

--- example4/nest_sim.py
+++ example4/nest_sim.py
@@ -7,9 +7,18 @@ import numpy as np
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 
+
+def get_current_offset(weight, rate, tau_m, tau_syn, C_m):
+    return weight / C_m * rate * tau_m * tau_syn * 1e-3
+
+
 simtime = 10000.
 tau_m = 1.
-tau_syn = 50.
+tau_syn = 20.
+C_m = 250.
+
+J = 200.
+max_rate = 50.
 
 # setup and simulate
 
@@ -20,10 +29,16 @@ nest.SetKernelStatus({'overwrite_files': True, 'resolution': 1.})
 music_in_proxy = nest.Create('music_event_in_proxy', 1, {'port_name': 'in'})
 music_out_proxy = nest.Create('music_event_out_proxy', 1, {'port_name': 'out'})
 
-neuron_left = nest.Create('iaf_psc_exp', 1, {'E_L': -59.5, 'V_th': -60., 'tau_m': tau_m, 'tau_syn_ex': tau_syn, 'tau_syn_in': tau_syn})
-neuron_right = nest.Create('iaf_psc_exp', 1, {'E_L': -60.5, 'V_th': -60., 'tau_m': tau_m, 'tau_syn_ex': tau_syn, 'tau_syn_in': tau_syn})
+neuron_left = nest.Create('iaf_psc_exp', 1, {
+    'E_L': -60.3 + get_current_offset(J, max_rate / 2., tau_m, tau_syn, C_m),
+    'V_th': -60., 'tau_m': tau_m, 'tau_syn_ex': tau_syn, 'tau_syn_in': tau_syn, 'C_m': C_m
+})
+neuron_right = nest.Create('iaf_psc_exp', 1, {
+    'E_L': -60. + get_current_offset(-J, max_rate / 2., tau_m, tau_syn, C_m),
+    'V_th': -60., 'tau_m': tau_m, 'tau_syn_ex': tau_syn, 'tau_syn_in': tau_syn, 'C_m': C_m
+})
 
-neuron_command = nest.Create('iaf_psc_exp', 1, {'E_L': -60., 'V_th': -60., 'tau_m': tau_m, 'tau_syn_ex': tau_syn, 'tau_syn_in': tau_syn})
+neuron_command = nest.Create('iaf_psc_exp', 1, {'E_L': -60., 'V_th': -60., 'tau_m': 10 * tau_m, 'tau_syn_ex': tau_syn, 'tau_syn_in': tau_syn})
 
 sd = nest.Create('spike_detector')
 
--- example4/zmq_sender.py
+++ example4/zmq_sender.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import json
-import math
 import time
 import zmq
 
