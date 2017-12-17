f52e7d95de8a59c50ecf7af8a51347bd105d00d7
Jakob Jordan <j.jordan@fz-juelich.de>
2017-12-17 12:24:31 +0100

    Update add_paths

--- add_paths.sh
+++ add_paths.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 export PATH=$HOME/opt/MUSIC/install/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/opt/MUSIC/install/lib:$LD_LIBRARY_PATH
+#
 export PATH=$HOME/opt/music-adapters/install/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/opt/music-adapters/install/lib:$LD_LIBRARY_PATH
+#
+export PATH=$HOME/opt/nest-simulator/install/bin:$PATH
 export PYTHONPATH=$HOME/opt/nest-simulator/install/lib/python3.5/site-packages:$PYTHONPATH
+#
+export PATH=$HOME/work/projects/python-gymz:$PATH
