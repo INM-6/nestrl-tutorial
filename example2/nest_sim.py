5831954eeab4c981edc1694e32fb31f652c0ab70
Jakob Jordan <jakobjordan@posteo.de>
2019-06-07 21:48:40 +0200

    Simplify zmq to nest example

--- example2/nest_sim.py
+++ example2/nest_sim.py
@@ -8,30 +8,19 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 
 simtime = 1500.
-tau_m = 1.
-tau_syn = 50.
 
 # setup and simulate
 
 nest.ResetKernel()
 
-nest.SetKernelStatus({'overwrite_files': True, 'resolution': 1.})
-
 music_in_proxy = nest.Create('music_event_in_proxy', 1, {'port_name': 'in'})
-
-neuron = nest.Create('iaf_psc_exp', 1, {'E_L': -65., 'V_th': -60., 'tau_m': tau_m, 'tau_syn_ex': tau_syn, 'tau_syn_in': tau_syn})
-dummy_neuron = nest.Create('iaf_psc_exp', 1, {'E_L': -65., 'V_th': 0., 'tau_m': tau_m, 'tau_syn_ex': tau_syn, 'tau_syn_in': tau_syn})
-
+neuron = nest.Create('iaf_psc_exp')
 sd = nest.Create('spike_detector')
-
 mv = nest.Create('multimeter', 1, {'record_from': ['V_m']})
 
-nest.Connect(music_in_proxy, neuron, syn_spec={'weight': 200.})
-nest.Connect(music_in_proxy, dummy_neuron, syn_spec={'weight': 200.})
-
+nest.Connect(music_in_proxy, neuron, syn_spec={'weight': 120.})
 nest.Connect(neuron, sd)
-
-nest.Connect(mv, dummy_neuron)
+nest.Connect(mv, neuron)
 
 comm.Barrier()  # necessary to synchronize with MUSIC
 
@@ -40,7 +29,6 @@ nest.Simulate(simtime)
 # plot results
 
 spikes = nest.GetStatus(sd, 'events')[0]
-
 vm = nest.GetStatus(mv, 'events')[0]
 
 fig = plt.figure()
@@ -49,9 +37,9 @@ ax = fig.add_subplot(111)
 ax.set_xlabel('Time (ms)')
 ax.set_ylabel('Membrane potential (mV)')
 ax.set_xlim([0., simtime])
-ax.set_ylim([-65., -50.])
+ax.set_ylim([-70., -45.])
 
-ax.plot(spikes['times'], [-55.] * len(spikes['times']), 'bo', label='Spikes', markeredgewidth=0, markersize=2)
+ax.plot(spikes['times'], [-48.] * len(spikes['times']), 'bo', label='Spikes', markeredgewidth=0, markersize=3)
 ax.plot(vm['times'], vm['V_m'], 'k', label='Free membrane potential')
 
 ax.legend(loc='upper right')
