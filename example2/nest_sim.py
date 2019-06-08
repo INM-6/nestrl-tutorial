5cc6149fe87758e6af3615cfa59c75b7f45c9650
Jakob Jordan <jakobjordan@posteo.de>
2019-06-08 13:54:27 +0200

    Update example2

--- example2/nest_sim.py
+++ example2/nest_sim.py
@@ -13,23 +13,22 @@ simtime = 1500.
 
 nest.ResetKernel()
 
-music_in_proxy = nest.Create('music_event_in_proxy', 1, {'port_name': 'in'})
+music_event_in = nest.Create('music_event_in_proxy', 1, {'port_name': 'in'})
 neuron = nest.Create('iaf_psc_exp')
 sd = nest.Create('spike_detector')
-mv = nest.Create('multimeter', 1, {'record_from': ['V_m']})
+m = nest.Create('multimeter', 1, {'record_from': ['V_m']})
 
-nest.Connect(music_in_proxy, neuron, syn_spec={'weight': 120.})
+nest.Connect(music_event_in, neuron, syn_spec={'weight': 120.})
 nest.Connect(neuron, sd)
-nest.Connect(mv, neuron)
+nest.Connect(m, neuron)
 
 comm.Barrier()  # necessary to synchronize with MUSIC
-
 nest.Simulate(simtime)
 
 # plot results
 
 spikes = nest.GetStatus(sd, 'events')[0]
-vm = nest.GetStatus(mv, 'events')[0]
+vm = nest.GetStatus(m, 'events')[0]
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
--- example2/nest_sim_rate.py
+++ example2/nest_sim_rate.py
@@ -7,25 +7,22 @@ import sys
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 
-simtime = 2000.
+simtime = 1500.
 
 # setup and simulate
 
 nest.ResetKernel()
 
-nest.SetKernelStatus({'overwrite_files': True, 'resolution': 1.0})
+nest.SetKernelStatus({'resolution': 1.0})  # TODO fix resolution issue
 
-music_in = nest.Create('music_rate_in_proxy', 1, {'port_name': 'in'})
-
+music_rate_in = nest.Create('music_rate_in_proxy', 1, {'port_name': 'in'})
 neuron = nest.Create('lin_rate_ipn', 1, {'mu': 1., 'sigma': 0.})
-
 m = nest.Create('multimeter', 1, {'record_from': ['rate']})
 
-nest.Connect(music_in, neuron, syn_spec={'model': 'rate_connection_instantaneous', 'weight': 1.})
+nest.Connect(music_rate_in, neuron, syn_spec={'model': 'rate_connection_instantaneous', 'weight': 1.})
 nest.Connect(m, neuron)
 
 comm.Barrier()  # necessary to synchronize with MUSIC
-
 nest.Simulate(simtime)
 
 # plot results
